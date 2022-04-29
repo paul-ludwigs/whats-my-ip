d3d61124034c2035a1053e421cb0f21786969180
Paul Ludwigs <paul.ludwigs@web.de>
2022-04-29 10:33:23 +0200

    Implement ipify and create env file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const API_KEY = process.env.REACT_APP_API_KEY;
+  const [ip, setIP] = useState("");
+
+
+  useEffect(()=>{
+    axios
+      .get(`https://geo.ipify.org/api/v2/country?apiKey=${API_KEY}`)
+      .then((response) => setIP(response.data.ip))
+      .catch((err) => console.log(err))
+    
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
-        
+        {ip ? <h1>What is my IP: {ip}</h1> : <h2>Loading...</h2>}
       </header>
     </div>
   );
